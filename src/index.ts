6ba92a1213ada8e4c7018a4464b9406bbc1bd0db
Fabrizio Lallo <f.lallo@2hire.io>
2021-03-11 22:58:56 +0100

    feat: changed server port

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 
 const HOST = process.env.HOST || "0.0.0.0";
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const start = async () => {
     try {
         await app.listen(PORT, HOST);
